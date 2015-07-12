ed537430566e9227d0d21183a69fcf1dd8f6f2f1
Keita Yamada <yshiro9@yahoo.co.jp>
2015-07-12 13:13:55 +0900

    first commit

---
+++ calculator.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title> 割り勘電卓</title>
+<style>
+body{
+  font-size: 16px;
+  text-align: center;
+  font-family: Arial, sans-serif;
+}
+h1{
+  font-size: 24px;
+}
+</style>
+</head>
+<body>
+</body>
+</html>
